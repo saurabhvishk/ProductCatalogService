2d71625f1b3a5339cea31fbbcb808a9280499267
saurabhvishk <isaurabhv@gmail.com>
2026-09-08 23:05:55 +0530

    adds DB integration using JPA and hybernate

---
+++ src/main/java/com/example/productcatalogservice/services/StorageProductService.java
@@ -0,0 +1,39 @@
+package com.example.productcatalogservice.services;
+
+import com.example.productcatalogservice.models.Product;
+import com.example.productcatalogservice.repos.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Primary
+public class StorageProductService implements IProductService{
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Override
+    public Product getProductById(Long id) {
+        Optional<Product> optionalProduct = productRepo.findById(id);
+        return optionalProduct.orElse(null);
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Product replaceProduct(Long id, Product product) {
+        return null;
+    }
+}
